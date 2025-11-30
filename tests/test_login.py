4c03970aa9b8b8585be70db228c4860585a9b409
Kirill Astrakhov <kirill__astrakhov@mail.ru>
2025-11-30 17:23:40 +0300

    Исправил тесты для входа. Но я не понимаю как исправить тесты на навигацию в конструкторе с начинками. Вы можете подсказать где мне поискать информацию что бы сделать все правильно

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,38 +1,49 @@
 from locators import StellarLocators as L
 from data import Data
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+# Функция для авторизации
 def login_flow(driver, email, password):
-    driver.find_element(*L.LOGIN_EMAIL_INPUT).send_keys(Data.TEST_EMAIL)
-    driver.find_element(*L.LOGIN_PASSWORD_INPUT).send_keys(Data.TEST_PASSWORD)
-    driver.find_element(*L.LOGIN_SUBMIT_BTN).click()
-    time.sleep(1)
-#Вход через кнопку 'Войти в аккаунт'
-def test_login_for_main(driver, test_user_credentials):
-    email, password, _ = test_user_credentials
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.visibility_of_element_located(L.LOGIN_EMAIL_INPUT)).send_keys(Data.TEST_EMAIL)
+    wait.until(EC.visibility_of_element_located(L.LOGIN_PASSWORD_INPUT)).send_keys(Data.TEST_PASSWORD)
+    wait.until(EC.element_to_be_clickable(L.LOGIN_SUBMIT_BTN)).click()
+    wait.until(EC.visibility_of_element_located(L.CONSTRUCTOR_BTN))
+
+# Вход через кнопку 'Войти в аккаунт'
+def test_login_for_main(driver):
+    email = Data.TEST_EMAIL
+    password = Data.TEST_PASSWORD
     driver.get(Data.STELLAR_URL)
     driver.find_element(*L.LOGIN_IN_ACCOUNT_BTN).click()
     login_flow(driver, email, password)
-    assert driver.find_element(*L.CONSTRUCTOR_BTN).is_displayed
-#Вход через кнопку "Личный кабинет"
-def test_login_from_personal_account(driver, test_user_credentials):
-    email, password, _ = test_user_credentials
+    assert driver.find_element(*L.CONSTRUCTOR_BTN).is_displayed()
+
+# Вход через кнопку "Личный кабинет"
+def test_login_from_personal_account(driver):
+    email = Data.TEST_EMAIL
+    password = Data.TEST_PASSWORD
     driver.get(Data.STELLAR_URL)
     driver.find_element(*L.PERSONAL_ACCOUNT_BTN).click()
     login_flow(driver, email, password)
     assert driver.find_element(*L.CONSTRUCTOR_BTN).is_displayed()
-#Вход через форму регистрации
-def test_login_from_registration_page(driver, test_user_credentials):
-    email, password, _ = test_user_credentials
+
+# Вход через форму регистрации
+def test_login_from_registration_page(driver):
+    email = Data.TEST_EMAIL
+    password = Data.TEST_PASSWORD
     driver.get(Data.STELLAR_URL)
     driver.find_element(*L.PERSONAL_ACCOUNT_BTN).click()
     driver.find_element(*L.REGISTER_LINK).click()
     driver.find_element(*L.RESTORE_LOGIN_LINK).click()
     login_flow(driver, email, password)
     assert driver.find_element(*L.CONSTRUCTOR_BTN).is_displayed()
-#Вход через востановление пароля
-def test_login_from_restore_password(driver, test_user_credentials):
-    email, password, _ = test_user_credentials
+
+# Вход через восстановление пароля
+def test_login_from_restore_password(driver):
+    email = Data.TEST_EMAIL
+    password = Data.TEST_PASSWORD
     driver.get(Data.STELLAR_URL)
     driver.find_element(*L.PERSONAL_ACCOUNT_BTN).click()
     driver.find_element(*L.RESTORE_PASSWORD_LINK).click()
