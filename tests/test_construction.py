b230b4afdb5ca8d319a9a1b508403f0b56045d53
Kirill Astrakhov <kirill__astrakhov@mail.ru>
2025-12-07 18:23:34 +0300

    Исправил тесты на конструктор

--- tests/test_construction.py
+++ tests/test_construction.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
 from locators import StellarLocators as L
 from data import Data
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,28 +7,23 @@ from selenium.webdriver.support import expected_conditions as EC
 #Проверка работы вкладок конструктора
 class TestConstructorTabs:
 
-    def open_constructor(self, driver, wait):
-        driver.get(Data.STELLAR_URL)
-        wait.until(EC.element_to_be_clickable(L.CONSTRUCTOR_BTN)).click()
+    def test_buns_cross(self, driver):
+        driver.find_element(*L.SAUCES_TAB).click()
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(*L.SAUCES_TAB))
+        driver.find_element(*L.BUNS_TAB)
+        buns_span = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(*L.BUNS_TAB))
 
-    def click_tab(self, driver, wait, element, expected_text):
-        tab = wait.until(EC.presence_of_element_located(element))
-        driver.execute_script("arguments[0].click();", tab)
-        wait.until(EC.text_to_be_present_in_element(L.ACTIVE_TAB, expected_text))
-        assert wait.until(EC.visibility_of_element_located(L.ACTIVE_TAB)).text == expected_text
+        assert buns_span.text == "Булки"
 
-    def test_go_to_buns_section(self, driver):
-        wait = WebDriverWait(driver, 10)
-        self.open_constructor(driver, wait)
-        self.click_tab(driver, wait, L.BUNS_TAB, "Булки")
+    def test_sauces_cross(self, driver):
+        driver.find_element(*L.SAUCES_TAB).click()
+        sauces_span = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(L.SAUCES_TAB))
 
-    def test_go_to_sauces_section(self, driver):
-        wait = WebDriverWait(driver, 10)
-        self.open_constructor(driver, wait)
-        self.click_tab(driver, wait, L.SAUCES_TAB, "Соусы")
+        assert sauces_span.text == "Соусы"
 
-    def test_go_to_fillings_section(self, driver):
-        wait = WebDriverWait(driver, 10)
-        self.open_constructor(driver, wait)
-        self.click_tab(driver, wait, L.FILLINGS_TAB, "Начинки")
+    def test_fillings_cross(self, driver):
+        driver.find_element(*L.FILLINGS_TAB).click()
+        fillings_span = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(L.FILLINGS_TAB))
+
+        assert fillings_span.text == "Начинки"
 
